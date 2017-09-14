c5afcd052f593671ff9bb828eac84d73f9dcd9f5
suganthcheenu <31839516+suganthcheenu@users.noreply.github.com>
2017-09-14 23:15:19 +0530

    my config and spec file updates

---
+++ Protractor/Spec.js
@@ -0,0 +1,7 @@
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+      browser.get('http://ui-staging.c1exchange.com/login');
+  
+      expect(browser.getTitle()).toEqual('C1X | Class One Exchange');
+    });
+  });
