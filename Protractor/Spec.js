515d36db7262df302fbdd406ea0c0a7ba353d2c2
suganthcheenu <31839516+suganthcheenu@users.noreply.github.com>
2017-09-14 23:34:07 +0530

    line to print the message in console

--- Protractor/Spec.js
+++ Protractor/Spec.js
@@ -3,5 +3,6 @@ describe('Protractor Demo App', function() {
       browser.get('http://ui-staging.c1exchange.com/login');
   
       expect(browser.getTitle()).toEqual('C1X | Class One Exchange');
+      document.write(browser.getTitle());
     });
   });
